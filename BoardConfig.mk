05a56121f5d16cb3be84fd4566b9262dfd786740
synescu <synescu@gmail.com>
2017-05-14 01:05:03 +0200

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,27 +8,45 @@ TARGET_CPU_ABI := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a9
 
-#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
+#PARTITION_SIZE
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x002800000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x002880000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x0DFFFC000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x05FD03C00
-BOARD_CACHEIMAGE_PARTITION_SIZE    := 0x1F4000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x5FDE03C00
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 0x01F7E8000
 BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_PLATFORM_DEVICE_BASE := /dev/block/platform/soc.0/
 
-TARGET_PREBUILT_KERNEL := device/lg/h840/kernel
 # Boot image
-BOARD_MKBOOTIMG_ARGS := --base 80000000 --pagesize 2048 --kernel_offset 00008000 --ramdisk_offset 02000000 --tags_offset 00000100   --cmdline 'console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.hardware=alicee vmalloc=300m'  
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x00000100
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := 'console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.hardware=alicee vmalloc=300m'
 
-TWHAVE_SELINUX := true
-#TW_USE_TOOLBOX := true
-TW_NO_EXFAT_FUSE := true
-TW_THEME := portrait_mdpi
-#RECOVERY_SDCARD_ON_DATA := true
-TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
-#TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/gadget/lun%d/file"
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+TW_INCLUDE_NTFS_3G := true
+
+# Kernel
+TARGET_PREBUILT_KERNEL := device/lg/h840/kernel  
+
+
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+RECOVERY_SDCARD_ON_DATA := true
+
+
 # TWRP specific build flags
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 149
+TW_INTERNAL_STORAGE_PATH := "/data/media/0"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+#TW_NO_EXFAT_FUSE := true
+TW_THEME := portrait_hdpi
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+
